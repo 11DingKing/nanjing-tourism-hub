a0921c586512483df28d5fed4b9594d49bb264e6
11DingKing <11DingKing@users.noreply.github.com>
2026-08-23 14:50:27 +0800

    private njtour-018

---
+++ internal/tourism/task18_support.go
@@ -0,0 +1,2 @@
+package tourism
+func TaskSupportExtra18() string{return "support"}
---
+++ internal/tourism/task18_test.go
@@ -0,0 +1,3 @@
+package tourism
+import("sync";"testing")
+func TestTestGuideClaimConflict(t *testing.T){s:=&TaskState{Limit:10};var w sync.WaitGroup;ch:=make(chan bool,2);for i:=0;i<2;i++{w.Add(1);go func(){defer w.Done();ch<-s.Run()}()};w.Wait();close(ch);n:=0;for x:=range ch{if x{n++}};if n!=1||s.Used>10{t.Fatal("oversold")}}
